bd4708f08bc37937f9f095360588e76dab2dd6b2
menachemH <mendelhalp@gmail.com>
2020-11-24 15:42:15 +0200

    add slideshow to middle secondery content

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+var slideIndex = 0;
+
+function showSlides(){
+    var i;
+    var slides = document.getElementsByClassName("my-slides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 8500);
+}
+
+
 function fnameBoxActive(){
     document.getElementById("fname").className += " was-submitted";
 }
