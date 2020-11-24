8f31b01a9331e2c5c641df9e24cf316eb57d50b3
menachemH <mendelhalp@gmail.com>
2020-11-24 13:06:10 +0200

    add email validation appearance

--- script.js
+++ script.js
@@ -12,4 +12,16 @@ function phoneBoxActive(){
 
 function emailBoxActive(){
     document.getElementById("email").className += " was-submitted";
+    var mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var inputText = document.getElementById("email-input").value;
+    var emailError = document.getElementById("email-error");
+    if (inputText.match(mailFormat)){
+        emailError.style.display = "none";
+    }
+    else if (inputText.length == 0){
+        emailError.style.display = "none";
+    }
+    else {
+        emailError.style.display = "initial";
+    }
 }
