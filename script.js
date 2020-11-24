a095439205c13216a15bd2b3bafc52501a269c73
menachemH <mendelhalp@gmail.com>
2020-11-24 12:33:44 +0200

    add validation to form

---
+++ script.js
@@ -0,0 +1,15 @@
+function fnameBoxActive(){
+    document.getElementById("fname").className += " was-submitted";
+}
+
+function lnameBoxActive(){
+    document.getElementById("lname").className += " was-submitted";
+}
+
+function phoneBoxActive(){
+    document.getElementById("phone").className += " was-submitted";
+}
+
+function emailBoxActive(){
+    document.getElementById("email").className += " was-submitted";
+}
